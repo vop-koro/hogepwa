eaeee11f1b88fc901acb12a0fd655016222607e9
vop-koro <koro@vop.co.jp>
2023-06-07 17:33:47 +0900

    ソースを追加

---
+++ dist/assets/index5-legacy-61496857.js
@@ -0,0 +1,5 @@
+System.register([],(function(e,n){"use strict";return{execute:function(){
+/*!
+			 * (C) Ionic http://ionicframework.com - MIT License
+			 */
+e("w","undefined"!=typeof window?window:void 0)}}}));
---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hogepwa/dist/sw.js', { scope: '/hogepwa/dist/' })})}
